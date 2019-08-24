c4bd59645f91dcaeb29bb9d8a24dff242c78a0c3
Max <maaxx.360@gmail.com>
2019-08-24 10:45:49 -0500

    Imagenes

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+*Autor: Adrian Maximiliano Munoz Martinez
+*Fecha: 20/10/1998
+*grupo: 
+*Horario: sabado 9am 11am
+*Matricula: 1679573
+*/
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
