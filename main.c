ecd9f409724bc642a315543b244530725e6da26a
mas <maaxx.360@gmail.com>
2019-09-14 10:39:12 -0500

    Practica6 act1, act2

--- main.c
+++ main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	/*
+	*Autor: Adrian Maximiliano Munoz Martinez
+	*Fecha: 31/08/2019
+	*grupo: 008
+	*Horario: sabado 9am 11am
+	*Matricula: 1679573
+	*/
 
 int main(int argc, char *argv[]) {
+	//******ACTIVIDAD 1******
+	int num, sum=0;
+	char keepG;
+	//Se ejecutara siempre que keepG sea diff a 's'
+	do{
+		printf("\nIntoduzca un numero: ");
+		scanf("%d", &num);
+		sum += num;
+		printf("Desea continuar(s/n): ");
+		//Limpia el cache antes de pedir un caracter
+		fflush(stdin);
+		scanf("%c", &keepG);
+	}while(keepG=='s');
+	printf("\nLa suma de los numeros introducidos es %d\n\n", sum);
+	printf("---------------");
+	//******ACTIVIDAD 2******
+	int numb=0;
+	//Se ejecutara siempre y cuando numb <= 100
+	while (numb<=100){
+		printf("\nIntroduzca un numero: ");
+		scanf("%d", &numb);
+		printf("Introduciste el numero %d\n", numb);
+	}
+	printf("\nIntroduciste un numero mayor a 100\n");
 	system("pause");
 	return 0;
 }
