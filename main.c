81cdec3433e50d6ad35e58735477f1a78627c1a2
Your Name <you@example.com>
2019-09-28 10:09:33 -0500

    Practica8 act1, act2, comments

--- main.c
+++ main.c
@@ -11,22 +11,29 @@
 
 int main(int argc, char *argv[]) {
 	//**************ACTIVIDAD1*****************
+	//Declarar arreglo de longitud 24
 	int numeroDeCoches[24], r;
 	int i;
+	//Llenar el arreglo
 	for(i=0;i<24;i++){
 		numeroDeCoches[i]=i+1;
 	}
+	//Guardar el valor 57 en la posicion 2 del arreglo
 	r=2;
 	numeroDeCoches[r] = 57;
 	printf("El numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
+	//Guardar el valor 57 en la posicion 2 del arreglo
 	r=21;
 	numeroDeCoches[r] = 57;
 	printf("\nEl numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
 	//**************ACTIVIDAD2*****************
+	//Declarar e inicializar variables A=8 y B=2
 	int A, B;
 	A=8;
 	B=4;
+	//Declarar r (el indice del arreglo) igual a A/B
 	r=A/B;
+	//Guardar el valor 57 en el indice A/B
 	numeroDeCoches[r] = 57;
 	printf("\nEl numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
 	printf("\n\n");
