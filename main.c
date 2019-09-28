29e53a4ee5a669c76cd8a910f571443c367278d0
Your Name <you@example.com>
2019-09-28 10:02:13 -0500

    Practica8 act1, act2

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+*Autor: Adrian Maximiliano Munoz Martinez
+*Fecha: 28/09/2019
+*Grupo: 008
+*Horario: 9-11am sabados
+*Matricula: 1679573
+*/
 
 int main(int argc, char *argv[]) {
+	//**************ACTIVIDAD1*****************
+	int numeroDeCoches[24], r;
+	int i;
+	for(i=0;i<24;i++){
+		numeroDeCoches[i]=i+1;
+	}
+	r=2;
+	numeroDeCoches[r] = 57;
+	printf("El numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
+	r=21;
+	numeroDeCoches[r] = 57;
+	printf("\nEl numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
+	//**************ACTIVIDAD2*****************
+	int A, B;
+	A=8;
+	B=4;
+	r=A/B;
+	numeroDeCoches[r] = 57;
+	printf("\nEl numero de coches en la hora %d es de %d", r, numeroDeCoches[r]);
+	printf("\n\n");
 	system("pause");
 	return 0;
 }
