ae933854b5f607caf4da4eb355b79bb05dab590a
Max <maaxx.360@gmail.com>
2019-10-12 20:01:48 -0500

    Practica10

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /*
 *Autor: Adrian Maximiliano Munoz Martinez
@@ -15,18 +16,35 @@ int main(int argc, char *argv[]) {
 	//**************ACTIVIDAD1*****************
 	char mana[' '];
 	int i, isPal=1;
-	
-	
+	int jumpSpace=0, jumpSpace1=0;
+	int leftIndex, rightIndex;
+
+
 	//Pedir entrada al usuario y guardarla en la vairable mana
 	printf("Ingresa un palindromo: ");
 	gets(mana);
-	
+
 	//Recorrer cadena simultaneamnete de incio a final y de final a incio
 	//Revisar que cada par de caracreres sea el mismo
-	//Si son diferenrtes el valor bandera sera 0 y se quedara en 0
-	for(i=0;i<strlen(mana)/2;i++){
-		if(mana[i]!=mana[strlen(mana)-i-1]){
-			isPal = 0; 
+	//Si son diferenrtes el valor bandera(isPal) sera 0 y se quedara en 0
+	leftIndex=-1;
+	rightIndex=strlen(mana);
+	for(i=0;i<strlen(mana);i++){
+		leftIndex++;
+		rightIndex--;
+
+		//Antes de evaluar la condicion revisar que no sea un espacio vacio, si lo es pasar al sisgueinte indice
+		while(mana[leftIndex]==' '){
+			leftIndex++;
+		}
+		while(mana[rightIndex]==' '){
+			rightIndex--;
+		}
+		//Salir del ciclo si se reccorio toda la cadena
+		if(rightIndex<0 || leftIndex>strlen(mana)-1);
+			break;
+		if(mana[leftIndex]!=mana[rightIndex]){
+			isPal = 0;
 		}
 	}
 	//Si el valor bandera es 1 la cadena es palindromo, si es 0 no es palindromo
@@ -46,13 +64,13 @@ int main(int argc, char *argv[]) {
 	char abc[26]="abcdefghijklmnopqrstuvwxyz";
 	char text[' '];
 	int counter[26]={};
-	
+
 	//Pedir frase y guardar entrada en variable text
 	printf("\n\nIngresa una frase: ");
 	gets(text);
-	
+
 	//Contar el numero de letras de cada tipo, recorriendo el texto y comparando cada letra con otro arreglo
-	//Si la letra de la cadena es igual la letra del arreglo en el indice j; sumar 1 al indice j del arreglo que contabiliza las letras
+	//Si la letra de la cadena es igual la letra del arreglo abc en el indice j; sumar 1 al indice j del arreglo que contabiliza las letras
 	for(i=0;i<strlen(text);i++){
 		for(j=0;j<26;j++){
 			if(tolower(text[i])==abc[j]){
@@ -60,7 +78,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	
+
 	//Imprimir el numero de veces que se repite cada letra recorriendo el arreglo de letras y el arreglo de contabilizaciones simultaneamente
 	printf("\nNumero de veces que se repite la letra");
 	for(i=0;i<26;i++){
