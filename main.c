08002805ef39c1b893fbcf6a0b9fa8847e89ce87
Max <maaxx.360@gmail.com>
2019-11-09 17:19:44 -0600

    Actividad 1

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE_NAME 30
 #define SIZE 3
@@ -16,19 +17,23 @@ int main(int argc, char *argv[]) {
 
 	struct picture_info{
 		char name[SIZE_NAME];
-		int date_time;
+		int date_time[3];
 	} pictures[SIZE], *pictures_pointer[SIZE];
 
 	//LLenar estuctura pictures
-	int i;
+	int i, j;
+	char *aux;
 	for(i=0;i<SIZE;i++){
 		getchar();
 		printf("\nFoto %d", i);
 		printf("\n\tNombre: ");
 		fflush(stdin);
 		fgets(pictures[i].name, SIZE_NAME, stdin);
-		printf("\tIngresa fecha: ");
-		scanf("%d", &pictures[i].date_time);
+		if ((aux = strchr(pictures[i].name, '\n')) != NULL) *aux = '\0';
+		do{
+			printf("\tIngresa la fecha en el siguiente formato dia/mes/ano (01/01/00): ");
+			scanf("%2d%*c%2d%*c%2d", &pictures[i].date_time[0], &pictures[i].date_time[1], &pictures[i].date_time[2]);
+		}while(0);
 	}
 
 
@@ -38,8 +43,18 @@ int main(int argc, char *argv[]) {
 	}
 
 	//Imprimir los informacion de las fotos desde la tabla de apuntadores
+	printf("\n\nReporte\n");
 	for(i=0;i<SIZE;i++){
-		printf("Foto\n\t%s %d\n", pictures_pointer[i]->name, pictures_pointer[i]->date_time);
+		printf("Foto %d\n\t Nombre: %s Fecha: ", i, pictures_pointer[i]->name);
+		for(j=0;j<3;j++){
+			printf("%d", pictures_pointer[i]->date_time[j]);
+			if(j<2){
+				printf("\\");
+			}
+			else{
+				printf("\n");
+			}
+		}
 	}
 	system("pause");
 	return 0;
