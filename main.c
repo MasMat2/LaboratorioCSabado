45346cadbf3267b9a4abe738251e3bf10682c425
Max <maaxx.360@gmail.com>
2019-11-09 23:30:59 -0600

    Activad 1, Actividad 2

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE_NAME 30
 #define SIZE 4
 
 /*
@@ -11,6 +12,7 @@
 *Horario: 9-11am sabados
 *Matricula: 1679573
 */
+
 struct coordinates{
 	int latitude;
 	int longitude;
@@ -20,8 +22,51 @@ void quick_sort(struct coordinates *array[], int min, int max, int latitude);
 int new_pivot(struct coordinates *array[], int min, int max, int latitude);
 
 int main(int argc, char *argv[]) {
+	//*************Actividad1*********
+	struct picture_info{
+		char name[SIZE_NAME];
+		int date_time[3];
+	} pictures[SIZE], *pictures_pointer[SIZE];
+
+	//LLenar estuctura pictures
+	int i, j;
+	char *aux;
+	for(i=0;i<SIZE;i++){
+		getchar();
+		printf("\nFoto %d", i);
+		printf("\n\tNombre: ");
+		fflush(stdin);
+		fgets(pictures[i].name, SIZE_NAME, stdin);
+		if ((aux = strchr(pictures[i].name, '\n')) != NULL) *aux = '\0';
+		do{
+			printf("\tIngresa la fecha en el siguiente formato dia/mes/ano (01/01/00): ");
+			scanf("%2d%*c%2d%*c%2d", &pictures[i].date_time[0], &pictures[i].date_time[1], &pictures[i].date_time[2]);
+		}while(0);
+	}
+
+
+	//Cada puntero i de la tabla pictures_pointer apuntara a la estructura pictures[i]
+	for(i=0;i<SIZE;i++){
+		pictures_pointer[i] = &pictures[i];
+	}
+
+	//Imprimir los informacion de las fotos desde la tabla de apuntadores
+	printf("\n\nReporte\n");
+	for(i=0;i<SIZE;i++){
+		printf("Foto %d\n\t Nombre: %s Fecha: ", i, pictures_pointer[i]->name);
+		for(j=0;j<3;j++){
+			printf("%d", pictures_pointer[i]->date_time[j]);
+			if(j<2){
+				printf("\\");
+			}
+			else{
+				printf("\n");
+			}
+		}
+	}
+
+	//*************Actividad2*********
 	struct coordinates places[SIZE], *as_latitude[SIZE], *as_longitude[SIZE];
-	int i;
 	for(i=0;i<SIZE;i++){
 		printf("\nLugar %d\n", i);
 		printf("\tIngresa la latitud: ");
