611ee9355aa95974e24283f989a255ca2466167f
Your Name <you@example.com>
2019-11-16 10:59:50 -0600

    Practia 15

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define STR_SIZE 100
 
 int main(int argc, char *argv[]) {
+	FILE *of, *ef;
+	char orig[]="origen.txt";
+	char salida[]="archivo_alreves.txt";
+	char corig[STR_SIZE];
+	char reves[STR_SIZE];
+	int i;
+	
+	of = fopen(orig, "r");
+	ef = fopen(salida, "w");
+	fgets(corig, STR_SIZE, of);
+	
+	for(i=0;i<strlen(corig);i++){
+		reves[i] = corig[strlen(corig)-1-i];
+	}
+	
+	fprintf(ef, "%s", reves);
+	//**************Actividad 2***********
+	FILE *nf, *sf;
+	int a, b;
+	char num[]="nums.txt";
+	char sum[]="suma.txt";
+	nf = fopen(num, "r");
+	sf = fopen(sum, "w");
+	
+	fscanf(nf, "%d %d", &a, &b);
+	fprintf(sf, "La suma de %d y %d es %d", a, b, a+b);
 	system("pause");
 	return 0;
 }
